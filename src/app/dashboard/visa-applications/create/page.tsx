541c8522f09f61e68fb43a3492fd1148f47213ce
amrit-niure <niureamrit2@gmail.com>
2024-09-19 17:41:45 +1000

    minor changes

---
+++ src/app/dashboard/visa-applications/create/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+interface RecordVisaApplicationProps {}
+
+const RecordVisaApplication: FC<RecordVisaApplicationProps> = ({}) => {
+  return <div> Record the visa application </div>;
+};
+
+export default RecordVisaApplication;
