6af6e096d6b95ba3ef13514114d7fffc52800b58
amrit-niure <niureamrit2@gmail.com>
2024-09-24 11:29:59 +1000

    feat: jrp and skills assestment schema added

---
+++ src/app/dashboard/job-ready-program/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface JRPProps {}
+
+const JRP: FC<JRPProps> = ({}) => {
+  return <div> Job Ready Program </div>;
+};
+
+export default JRP;
---
+++ src/app/dashboard/skills-assesment/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";interface SkillsAssesmentProps {}
+
+const SkillsAssesment: FC<SkillsAssesmentProps> = ({}) => {
+  return <div> Skills Assesment Page </div>;
+};
+
+export default SkillsAssesment;
