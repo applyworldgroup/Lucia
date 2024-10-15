28ba18f3ae8c995bf6253acf2ce836c29ec90432
amrit-niure <niureamrit2@gmail.com>
2024-10-15 16:09:33 +1100

    feat: team members added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,10 @@
 // src/lib/lucia.ts
-import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
-import { Lucia } from 'lucia';
-import prisma from './db'
-import { cache } from 'react';
-import { cookies } from 'next/headers';
-import { Role } from '@prisma/client';
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia } from "lucia";
+import prisma from "./db";
+import { cache } from "react";
+import { cookies } from "next/headers";
+import { Role } from "@prisma/client";
 
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
 
@@ -15,7 +15,7 @@ export const lucia = new Lucia(adapter, {
     expires: false,
     attributes: {
       // set to `true` when using HTTPS
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
     },
   },
   getUserAttributes: (attributes) => {
@@ -23,37 +23,45 @@ export const lucia = new Lucia(adapter, {
       email: attributes.email,
       role: attributes.role,
     };
-
   },
 });
 
 export const validateRequest = cache(async () => {
   const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
 
-  if (!sessionId) return {
-    user: null,
-    session: null
-  }
+  if (!sessionId)
+    return {
+      user: null,
+      session: null,
+    };
   const { user, session } = await lucia.validateSession(sessionId);
   try {
     if (session && session.fresh) {
       const sessionCookie = lucia.createSessionCookie(session.id);
-      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
     }
     if (!session) {
       const sessionCookie = lucia.createBlankSessionCookie();
-      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
     }
   } catch {
     // Next.js throws error when attempting to set cookies when rendering page
   }
   return {
     user,
-    session
+    session,
   };
 });
 
-declare module 'lucia' {
+declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
     DatabaseUserAttributes: DatabaseUserAttributes;
@@ -62,5 +70,5 @@ declare module 'lucia' {
 
 interface DatabaseUserAttributes {
   email: string;
-  role: Role
+  role: Role;
 }
